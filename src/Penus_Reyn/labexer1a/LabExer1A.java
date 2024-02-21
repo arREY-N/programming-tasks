5b09f3fc338e93e91de20923d5fdf0b217c16524
Reyn Penus <reypenus1224@gmail.com>
2024-02-21 09:23:52 +0800

    Use for loop instead of for-each loop

--- src/Penus_Reyn/labexer1a/LabExer1A.java
+++ src/Penus_Reyn/labexer1a/LabExer1A.java
@@ -3,7 +3,7 @@ package Penus_Reyn.labexer1a;
 public class LabExer1A {
     public static void main(String[] args) {
         int faveNumber = 1224;
-        String faveCartChar = "Ben 10";
+        String faveCartChar = "Ben10";
         char mi = 'M';
         char[] nickNameArray = {'R', 'e', 'y', 'n'};
 
@@ -12,10 +12,17 @@ public class LabExer1A {
         System.out.println("My name is Reynal " + mi +". Penus.");
         System.out.print("You can call me ");
 
-        for(char letter: nickNameArray){
-            System.out.print(letter);
+        for(int i = 0; i<nickNameArray.length; i++){
+            System.out.print(nickNameArray[i]);
         }
 
         System.out.println(".");
+
+        /*  
+            an alternative is to use for-each loop to display all the elements inside the array
+            for(char letter: nickNameArray){
+             System.out.print(letter);
+            }
+        */
     }
 }
